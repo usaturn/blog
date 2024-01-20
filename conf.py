8519094a8178939b4c08ea6d47c14ac13094ae1f
Go.YAMADA <kane@berserga.com>
2024-01-20 12:06:58 +0900

    Feature/update blog config (#1)
    * update blog config
    
    * fix
    
    * update config
    
    * fix

--- conf.py
+++ conf.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python
 
-# test build configuration file, created by
-# `ablog start` on Sat Sep  3 15:09:13 2016.
+
+# usaturnblog build configuration file, created by
+# `ablog start` on Thu Nov 30 22:15:01 2023.
 #
 # Note that not all possible configuration values are present in this file.
 # All configuration values have a default; values that are commented out
@@ -16,18 +16,18 @@ import alabaster
 # -- General ABlog Options ----------------------------------------------------
 
 # A path relative to the configuration directory for blog archive pages.
-#blog_path = 'blog'
+# blog_path = 'blog'
 
-# The title for the blog, used in acthive pages.  Default is ``'Blog'``.
-blog_title = u'星のダンスを見においで'
+# The "title" for the blog, used in active pages.  Default is ``'Blog'``.
+blog_title = "星のダンスを見においで"
 
 # Base URL for the website, required for generating feeds.
 # e.g. blog_baseurl = "http://example.com/"
-blog_baseurl = u'https://blog.usaturn.net/'
+blog_baseurl = "https://blog.usaturn.net/"
 
 # Choose to archive only post titles. Archiving only titles can speed
 # up project building.
-#blog_archive_titles = False
+# blog_archive_titles = False
 
 # -- Blog Authors, Languages, and Locations -----------------------------------
 
@@ -35,7 +35,7 @@ blog_baseurl = u'https://blog.usaturn.net/'
 # links. Dictionary keys are what should be used in ``post`` directive
 # to refer to the author.  Default is ``{}``.
 blog_authors = {
-    'usaturn': ('usaturn', None),
+    "usaturn": ("usaturn", None),
 }
 
 
@@ -43,12 +43,12 @@ blog_authors = {
 # links of these languages. Similar to :confval:`blog_authors`, dictionary
 # keys should be used in ``post`` directive to refer to the locations.
 # Default is ``{}``.
-#blog_languages = {
+# blog_languages = {
 #    'en': ('English', None),
-#}
+# }
 
 blog_languages = {
-    'ja': ('Japanese', None),
+    "ja": ("Japanese", None),
 }
 
 
@@ -56,37 +56,52 @@ blog_languages = {
 # links of these locations. Similar to :confval:`blog_authors`, dictionary
 # keys should be used in ``post`` directive to refer to the locations.
 # Default is ``{}``.
-#blog_locations = {
-#    'Earth': ('The Blue Planet', 'http://en.wikipedia.org/wiki/Earth),
-#}
+# blog_locations = {
+#    'Earth': ('The Blue Planet', 'https://en.wikipedia.org/wiki/Earth),
+# }
 
+# This will prevent ablog from injecting its own templates into the Sphinx
+# build. This is only useful when you have a custom template bridge (rare).
+# See https://github.com/sunpy/ablog/pull/144 for the full context.
+# skip_injecting_base_ablog_templates = False
 
 # -- Blog Post Related --------------------------------------------------------
 
-# post_date_format = '%b %d, %Y'
-post_date_format = '%Y/%m/%d'
-
+# Format date for a post.
+# post_date_format = '%%b %%d, %%Y'
+post_date_format = "%Y/%m/%d"
 
 # Number of paragraphs (default is ``1``) that will be displayed as an excerpt
 # from the post. Setting this ``0`` will result in displaying no post excerpt
 # in archive pages.  This option can be set on a per post basis using
-#post_auto_excerpt = 1
+# post_auto_excerpt = 1
 
 # Index of the image that will be displayed in the excerpt of the post.
 # Default is ``0``, meaning no image.  Setting this to ``1`` will include
 # the first image, when available, to the excerpt.  This option can be set
 # on a per post basis using :rst:dir:`post` directive option ``image``.
-#post_auto_image = 0
+# post_auto_image = 0
 
 # Number of seconds (default is ``5``) that a redirect page waits before
 # refreshing the page to redirect to the post.
-#post_redirect_refresh = 5
+# post_redirect_refresh = 5
 
 # When ``True``, post title and excerpt is always taken from the section that
 # contains the :rst:dir:`post` directive, instead of the document. This is the
 # behavior when :rst:dir:`post` is used multiple times in a document. Default
 # is ``False``.
-#post_always_section = False
+# post_always_section = False
+
+# When ``True``, links to the previous and next posts will be rendered at the
+# bottom of the page.
+# Default is ``True``
+# post_show_prev_next = True
+
+# When ``False``, the :rst:dir:`orphan` directive is not automatically set
+# for each post. Without this directive, Sphinx will warn about posts that
+# are not explicitly referenced via another document. :rst:dir:`orphan` can
+# be set on a per-post basis as well if this is false. Default is ``True``.
+post_auto_orphan = True
 
 # -- ABlog Sidebars -------------------------------------------------------
 
@@ -97,36 +112,54 @@ post_date_format = '%Y/%m/%d'
 # In addition, there are authors.html, languages.html, and locations.html
 # sidebars that link to author and location archive pages.
 html_sidebars = {
-    '**': [ 'about.html',
-            'postcard.html',
-            #'navigation.html',
-            #'recentposts.html',
-            'tagcloud.html',
-            'categories.html',  'archives.html',
-            'searchbox.html',
-            ],
-    }
-
+    "**": [
+        "about.html",
+        "ablog/postcard.html",
+        # "navigation.html",
+        # "ablog/recentposts.html",
+        "ablog/tagcloud.html",
+        "ablog/categories.html",
+        "ablog/archives.html",
+        "searchbox.html",
+    ],
+}
 
 # -- Blog Feed Options --------------------------------------------------------
 
 # Turn feeds by setting :confval:`blog_baseurl` configuration variable.
 # Choose to create feeds per author, location, tag, category, and year,
 # default is ``False``.
-#blog_feed_archives = False
+# blog_feed_archives = False
 
 # Choose to display full text in blog feeds, default is ``False``.
-#blog_feed_fulltext = False
+# blog_feed_fulltext = False
 
 # Blog feed subtitle, default is ``None``.
-#blog_feed_subtitle = None
+# blog_feed_subtitle = None
 
 # Choose to feed only post titles, default is ``False``.
-#blog_feed_titles = False
+# blog_feed_titles = False
+
+# Specify custom Jinja2 templates for feed entry elements:
+#     `title`, `summary`, or `content`
+# For example, to add an additional feed for posting to social media:
+# blog_feed_templates = {
+#     # Use defaults, no templates
+#     "atom": {},
+#     # Create content text suitable posting to social media
+#     "social": {
+#         # Format tags as hashtags and append to the content
+#         "content": "{ title }{% for tag in post.tags %}"
+#         " #{ tag.name|trim()|replace(' ', '') }"
+#         "{% endfor %}",
+#     },
+# }
+# Default: Create one `atom.xml` feed without any templates
+# blog_feed_templates = {"atom": {} }
 
 # Specify number of recent posts to include in feeds, default is ``None``
 # for all posts.
-#blog_feed_length = None
+# blog_feed_length = None
 
 # -- Font-Awesome Options -----------------------------------------------------
 
@@ -134,115 +167,110 @@ html_sidebars = {
 # is ``True``
 
 # Link to `Font Awesome`_ at `Bootstrap CDN`_ and use icons in sidebars
-# and post footers.  Default: ``False``
+# and post footers.  Default: ``None``
 fontawesome_link_cdn = True
 
 # Sphinx_ theme already links to `Font Awesome`_.  Default: ``False``
-#fontawesome_included = False
+# fontawesome_included = False
 
 # Alternatively, you can provide the path to `Font Awesome`_ :file:`.css`
 # with the configuration option: fontawesome_css_file
 # Path to `Font Awesome`_ :file:`.css` (default is ``None``) that will
 # be linked to in HTML output by ABlog.
-#fontawesome_css_file = None
+# fontawesome_css_file = None
 
 # -- Disqus Integration -------------------------------------------------------
 
 # You can enable Disqus_ by setting ``disqus_shortname`` variable.
 # Disqus_ short name for the blog.
-disqus_shortname = 'usaturn'
-#disqus_shortname = 'Comment'
+# disqus_shortname = 'usaturn'
 
 # Choose to disqus pages that are not posts, default is ``False``.
-#disqus_pages = False
-disqus_pages = True
+# disqus_pages = True
 
 # Choose to disqus posts that are drafts (without a published date),
 # default is ``False``.
-#disqus_drafts = True
+# disqus_drafts = False
 
 # -- Sphinx Options -----------------------------------------------------------
 
 # If your project needs a minimal Sphinx version, state it here.
-needs_sphinx = '1.2'
+needs_sphinx = "1.2"
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.extlinks',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.todo',
-    'alabaster',
-    'ablog',
+    "sphinx.ext.extlinks",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.todo",
+    "alabaster",
+    "ablog",
 ]
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates', ablog.get_html_templates_path()]
-
 # The suffix(es) of source filenames.
-source_suffix = '.rst'
+source_suffix = ".rst"
 
 # The encoding of source files.
-#source_encoding = 'utf-8-sig'
+# source_encoding = 'utf-8-sig'
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = "index"
 
 # General information about the project.
-project = u'星のダンスを見においで'
-copyright = u'2016～2021, usaturn'
-author = u'usaturn'
+project = "星のダンスを見においで"
+copyright = "2012～2023, usaturn"
+author = "usaturn"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
 # built documents.
 #
 # The short X.Y version.
-version = ''
+version = ""
 # The full version, including alpha/beta/rc tags.
-release = ''
+release = ""
 
 # The language for content autogenerated by Sphinx. Refer to documentation
 # for a list of supported languages.
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = 'ja'
+language = "ja"
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
-#today = ''
+# today = ''
 # Else, today_fmt is used as the format for a strftime call.
-#today_fmt = '%B %d, %Y'
+# today_fmt = '%%B %%d, %%Y'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = []
+exclude_patterns = [""]
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
-#default_role = None
+# default_role = None
 
 # If true, '()' will be appended to :func: etc. cross-reference text.
-#add_function_parentheses = True
+# add_function_parentheses = True
 
 # If true, the current module name will be prepended to all description
 # unit titles (such as .. function::).
-#add_module_names = True
+# add_module_names = True
 
 # If true, sectionauthor and moduleauthor directives will be shown in the
 # output. They are ignored by default.
-#show_authors = False
+# show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 
 # A list of ignored prefixes for module index sorting.
-#modindex_common_prefix = []
+# modindex_common_prefix = []
 
 # If true, keep warnings as "system message" paragraphs in the built documents.
-#keep_warnings = False
+# keep_warnings = False
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
@@ -252,111 +280,95 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-    'github_button': True,
+    "github_button": True,
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = [alabaster.get_path()]
 
-
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+# html_title = None
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-#html_short_title = None
+# html_short_title = None
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = 'images/usaturn_logo.png'
+html_logo = "images/usaturn_logo.png"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = 'images/usaturn_icon.png'
+html_favicon = "images/usaturn_icon.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
 # directly to the root of the documentation.
-#html_extra_path = []
+# html_extra_path = []
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
-html_last_updated_fmt = '%Y/%m/%d'
+html_last_updated_fmt = "%Y/%m/%d"
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
-#html_use_smartypants = True
-
-# Custom sidebar templates, maps document names to template names.
-#html_sidebars = {}
+# html_use_smartypants = True
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
-#html_additional_pages = {}
+# html_additional_pages = {}
 
 # If false, no module index is generated.
-#html_domain_indices = True
+# html_domain_indices = True
 
 # If false, no index is generated.
-#html_use_index = True
+# html_use_index = True
 
 # If true, the index is split into individual pages for each letter.
-#html_split_index = False
+# html_split_index = False
 
 # If true, links to the reST sources are added to the pages.
-#html_show_sourcelink = True
+# html_show_sourcelink = True
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
-#html_show_sphinx = True
+# html_show_sphinx = True
 
 # If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
-#html_show_copyright = True
+# html_show_copyright = True
 
 # If true, an OpenSearch description file will be output, and all pages will
 # contain a <link> tag referring to it.  The value of this option must be the
 # base URL from which the finished HTML is served.
-#html_use_opensearch = ''
+# html_use_opensearch = ''
 
 # This is the file name suffix for HTML files (e.g. ".xhtml").
-#html_file_suffix = None
+# html_file_suffix = None
 
 # Language to be used for generating the HTML full-text search index.
 # Sphinx supports the following languages:
 #   'da', 'de', 'en', 'es', 'fi', 'fr', 'hu', 'it', 'ja'
 #   'nl', 'no', 'pt', 'ro', 'ru', 'sv', 'tr'
-html_search_language = 'ja'
+html_search_language = "ja"
 
 # A dictionary with options for the search language support, empty by default.
 # Now only 'ja' uses this config value
-#html_search_options = {'type': 'default'}
+# html_search_options = {'type': 'default'}
 
 # The name of a javascript file (relative to the configuration directory) that
 # implements a search results scorer. If empty, the default will be used.
-#html_search_scorer = 'scorer.js'
+# html_search_scorer = 'scorer.js'
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'testdoc'
-
-# 2a. if `templates_path` is not defined
-#templates_path = [ablog.get_html_templates_path()]
-
-# 2b. if `templates_path` is defined
-templates_path.append(ablog.get_html_templates_path())
-
-
-#import alabastermobile
-#html_theme = 'alabastermobile'
-#html_theme_path = [alabastermobile.get_path()]
-
+htmlhelp_basename = "usaturnblogdoc"
